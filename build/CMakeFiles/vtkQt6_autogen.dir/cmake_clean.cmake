d928169b0f8e99d7d4068b4a22d721359ee0ab2c
Sekar Govindarajan <sekar.govindarajan@gmail.com>
2020-12-31 14:58:33 -0500

    Get rid of the build dir

--- build/CMakeFiles/vtkQt6_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/vtkQt6_autogen"
-  "vtkQt6_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/vtkQt6_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
