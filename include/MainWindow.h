03951c733b4ed5ad7aa12fa9ea110c21fd5991c3
Sekar Govindarajan <sekar.govindarajan@gmail.com>
2020-12-31 15:40:49 -0500

    Reindent lines

--- include/MainWindow.h
+++ include/MainWindow.h
@@ -13,32 +13,31 @@ template<typename T>
 using vtkPtr = vtkSmartPointer<T>;
 
 namespace Ui {
-class MainWindow;
+  class MainWindow;
 }
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+  Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+  explicit MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
 
 public slots:
-    void onDrawSphereClick();
-    void onPickColor();
+  void onDrawSphereClick();
+  void onPickColor();
 
 private:
-    Ui::MainWindow *ui;
+  Ui::MainWindow *ui;
 
-    vtkPtr<vtkGenericOpenGLRenderWindow> m_renderWindow;
-    vtkPtr<vtkRenderer> m_renderer;
-    vtkPtr<QVTKInteractor> m_interactor;
-    vtkPtr<vtkInteractorStyleSwitch> m_interactorStyle;
+  vtkPtr<vtkGenericOpenGLRenderWindow> m_renderWindow;
+  vtkPtr<vtkRenderer> m_renderer;
+  vtkPtr<QVTKInteractor> m_interactor;
+  vtkPtr<vtkInteractorStyleSwitch> m_interactorStyle;
 
-    // state of Background and sphere colors
-    float m_bgcR, m_bgcG, m_bgcB;
-    float m_sphR, m_sphG, m_sphB;
+  // state of Background and sphere colors
+  float m_bgcR, m_bgcG, m_bgcB;
+  float m_sphR, m_sphG, m_sphB;
 };
 
 #endif // MAINWINDOW_H
